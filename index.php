91daf1754a5b9f015dd3acd921c35dd5fbd638f9
Евгений Кириллов на хостинге <477504@mail.ru>
2018-11-08 12:49:08 +0300

    Start git repository

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Парк ЛПК</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <style>
+  </style>
+</head>
+<body>
+<h1>Парк ЛПК</h1>
+</body>
+</html>
