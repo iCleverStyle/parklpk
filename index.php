54b5fc9faa18a33eefcbae0c80e119124733b2e0
Евгений Кириллов - дома <477504@mail.ru>
2018-11-08 17:16:40 +0300

    Добавил лого и отцентровал

--- index.php
+++ index.php
@@ -6,9 +6,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <style>
+      h1 {
+         font-family: sans-serif;
+         text-align: center;
+      }
+      .img-c {
+         margin: 0 auto;
+         width: 200px;
+         display: block;
+      }
+      .main {
+         max-width: 1280px;
+         margin: 0 auto;
+      }
   </style>
 </head>
-<body>
-<h1>Парк ЛПК</h1>
+<body style="100%">
+<div class="main">
+   <h1>Парк ЛПК</h1>
+   <img class="img-c" src="logo1.jpg">
+</div>
 </body>
 </html>
