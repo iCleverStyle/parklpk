61c8adde7e31f46b850463d8893433e29632fbd9
Evgeny Kirillov <477504@gmail.com>
2018-11-10 21:29:16 +0300

    Поправил тайтл и добавил дискрипшн

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
-  <title>Парк ЛПК</title>
+  <title>Парк ЛПК в Нововятске</title>
+  <meta name="description" content="Участвуй в конкурсе проектов благоустройства Парка ЛПК. Выйграйте денежный приз и получите проект в портфолио."/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <style>
